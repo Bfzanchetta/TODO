f0884b0f55eec469b3ed802ba84e75c164c489a2
Bfzanchetta <bfzanchetta@hotmail.com>
2016-06-07 17:51:13 -0300

    Update Perceptron.java

--- Perceptron.java
+++ Perceptron.java
@@ -9,53 +9,69 @@ import java.util.Random;
  * @author Breno Zanchetta
  */
 public class Neuron {
+    
+    ArrayList<Double> x = new ArrayList<>(10);
+    ArrayList<Double> w = new ArrayList<>(10);
+    ArrayList<Double> y = new ArrayList<>(10);
+    ArrayList<Double> b = new ArrayList<>(10);
 
-    public static void main(String[] args){
-        ArrayList<Double> x = new ArrayList<>(10);
-        ArrayList<Double> w = new ArrayList<>(10);
-        ArrayList<Double> y = new ArrayList<>(10);
-        ArrayList<Double> b = new ArrayList<>(10);
-        
+    public void treinaNeuronio(){
+        int count = 0;
         //Preenchendo o Array x com valores de entrada
         for(int i=0; i<10; i++){
             x.add(Math.random()*0.5);
-            System.out.println(x.get(i));
+            //System.out.println(x.get(i));
         }
-        
+            
         //Preenchendo o Array w com valores aleatorios entre 0 e 1
         for(int i=0; i<10; i++){
             w.add(Math.random());
-            System.out.println(w.get(i));
+            //System.out.println(w.get(i));
         }
-        
+            
         //Preenchendo o Array de bias com valores aleatorios entre 0 e 0.2
         for(int i=0; i<10; i++){
             b.add(Math.random()*0.2);
-            System.out.println(b.get(i));
+            //System.out.println(b.get(i));
         }
-        
-        
+       
         double resultado = 0.0;
         double erro = 0.0;
         double desejado = 1.0;
         double alfa = 0.1;
         
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
-                resultado += (x.get(j) * w.get(j)) + b.get(j);
-                //y.add(i, (x.get(j) * w.get(j)) + b.get(j));
+        while(count!=1000){
+            for(int i=0; i<10; i++){
+                for(int j=0; j<10; j++){
+                    resultado += (x.get(j) * w.get(j)) + b.get(j);
+                    //y.add(i, (x.get(j) * w.get(j)) + b.get(j));
+                }
+                //System.out.println("RESULTADO: "+ resultado);
+                //Atualiza os pesos e os bias
+                erro = Math.abs(desejado - resultado);
+                //System.out.println("Erro eh: "+erro);
+                for(int j=0; j<10; j++){
+                    w.add(i+1, w.get(i) + (alfa * erro * x.get(j)));
+                    b.add(i+1, b.get(i) + alfa * erro);
+                    alfa = alfa*0.98;
+                }
             }
-            System.out.println("RESULTADO: "+ resultado);
-            //Atualiza os pesos e os bias
-            erro = Math.abs(desejado - resultado);
-            System.out.println("Erro eh: "+erro);
-            for(int j=0; j<10; j++){
-                w.add(i+1, w.get(i) + (alfa * erro * x.get(j)));
-                b.add(i+1, b.get(i) + alfa * erro);
+            for(int i=0; i<10; i++){
+                System.out.println("O peso numero "+i+" eh:"+ w.get(i));
             }
+            y.add(resultado*0.9);
+        count++;
+        System.out.println(count+" interacao");
         }
-        for(int i=0; i<10; i++){
-            System.out.println("O peso numero "+i+" eh:"+ w.get(i));
-        }
+        
+        System.out.println("Agora printando o resultado");
+            for(int i=0; i<10; i++){
+            System.out.println(y.get(i));
+        } 
+    }
+    
+    public void gradienteDescendente(){
+        
     }
 }
+    
