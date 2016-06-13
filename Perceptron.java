61c30b61ee2634b53e18f3ef1887f04ae3214f30
Bfzanchetta <bfzanchetta@hotmail.com>
2016-06-13 17:42:50 -0300

    Update Perceptron.java

--- Perceptron.java
+++ Perceptron.java
@@ -1,5 +1,9 @@
 package neuron;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import static java.lang.Math.exp;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,20 +14,47 @@ import java.util.Random;
  * @author Breno Zanchetta
  */
 public class Neuron {
-    boolean treinamento = false;
     
-    ArrayList<Double> x = new ArrayList<>(10);
+    boolean treinamento = false;
+    ArrayList<String> x1 = new ArrayList<>(10);
+    ArrayList<String> x2 = new ArrayList<>(10);
+    ArrayList<String> x3 = new ArrayList<>(10);
+    ArrayList<String> t = new ArrayList<>();
+    
+    Double y=0.0;
     ArrayList<Double> w = new ArrayList<>(10);
-    ArrayList<Double> y = new ArrayList<>(10);
     ArrayList<Double> b = new ArrayList<>(10);
 
+    public Neuron() {
+    }
+    
+    public void leEntrada() throws FileNotFoundException, IOException{
+        FileReader file = new FileReader("/home/nautec/NetBeansProjects/Neuron/src/neuron/entrada.txt");
+        BufferedReader lerArq = new BufferedReader(file);
+        
+        String linha = lerArq.readLine(); // lê a primeira linha
+        
+        while(linha != null) { 
+            x1.add(linha.substring(0, 4));
+            x2.add((linha.substring(4, 9)));
+            x3.add((linha.substring(9, 14)));
+            t.add((linha.substring(14, linha.length())));
+            System.out.printf("%s\n", linha);
+            linha = lerArq.readLine(); // lê da segunda até a última linha
+        }
+        System.out.println(""+file);
+        
+        
+        
+    }
+    
     public void treinaNeuronio(){
         int count = 0;
         //Preenchendo o Array x com valores de entrada
-        for(int i=0; i<10; i++){
-            x.add(Math.random()*0.5);
+        //for(int i=0; i<10; i++){
+          //  x.add(Math.random()*0.5);
             //System.out.println(x.get(i));
-        }
+        //}
             
         //Preenchendo o Array w com valores aleatorios entre 0 e 1
         for(int i=0; i<10; i++){
@@ -45,7 +76,9 @@ public class Neuron {
         while(count!=1000){
             for(int i=0; i<10; i++){
                 for(int j=0; j<10; j++){
-                    resultado += (x.get(j) * w.get(j)) + b.get(j);
+                    resultado += (Double.parseDouble(x1.get(j))* w.get(j)) + b.get(j);
+                    resultado += (Double.parseDouble(x2.get(j))* w.get(j)) + b.get(j);
+                    resultado += (Double.parseDouble(x3.get(j))* w.get(j)) + b.get(j);
                     //y.add(i, (x.get(j) * w.get(j)) + b.get(j));
                 }
                 //System.out.println("RESULTADO: "+ resultado);
@@ -61,15 +94,10 @@ public class Neuron {
             for(int i=0; i<10; i++){
                 System.out.println("O peso numero "+i+" eh:"+ w.get(i));
             }
-            y.add(resultado*0.9);
+            y=resultado*0.9;
         count++;
         System.out.println(count+" interacao");
         }
-        
-        System.out.println("Agora printando o resultado");
-            for(int i=0; i<10; i++){
-            System.out.println(y.get(i));
-        } 
     treinamento = true;        
     }
     
@@ -83,3 +111,4 @@ public class Neuron {
         return f;
     }
 }
+    
