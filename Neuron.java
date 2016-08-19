b479ed60fe8d24e9da2ca0a64d7d5cdd931c0c5b
Bfzanchetta <bfzanchetta@hotmail.com>
2016-08-19 14:15:28 -0300

    Update Neuron.java

--- Neuron.java
+++ Neuron.java
@@ -14,33 +14,33 @@ import java.util.Random;
  * @author Breno Zanchetta
  */
 public class Neuron {
-    
+
     boolean treinamento = false;
     ArrayList<Double> x = new ArrayList<>(10);
     ArrayList<Double> w = new ArrayList<>(10);
-    Double y=0.0;
+    Double y = 0.0;
     ArrayList<Double> b = new ArrayList<>(10);
 
     public Neuron() {
     }
-    
-    public Neuron(ArrayList<Double> x) throws IOException{
+
+    public Neuron(ArrayList<Double> x) throws IOException {
         this.x = x;
         this.leEntrada();
     }
-    
-    public void leEntrada() throws FileNotFoundException, IOException{
-        FileReader file = new FileReader("/home/nautec/NetBeansProjects/Neuronio/src/neuronio/entrada.txt");
+
+    public void leEntrada() throws FileNotFoundException, IOException {
+        FileReader file = new FileReader("/home/breno/NetBeansProjects/Neuronio/src/neuronio/entrada.txt");
         BufferedReader lerArq = new BufferedReader(file);
-        
+
         String linha = lerArq.readLine(); // lê a primeira linha
-        
+
         ArrayList<String> x1 = new ArrayList<>();
         ArrayList<String> x2 = new ArrayList<>();
         ArrayList<String> x3 = new ArrayList<>();
         ArrayList<String> t = new ArrayList<>();
-        
-        while(linha != null) { 
+
+        while (linha != null) {
             x1.add((linha.substring(0, 4)));
             x2.add((linha.substring(4, 9)));
             x3.add((linha.substring(9, 14)));
@@ -48,37 +48,40 @@ public class Neuron {
             System.out.printf("%s\n", linha);
             linha = lerArq.readLine(); // lê da segunda até a última linha
         }
-        System.out.println(""+file);
+        System.out.println("" + file);
     }
-    
-    public void treinaNeuronio(){
-        int count = 0;
-        //Preenchendo o Array x com valores de entrada
-        for(int i=0; i<10; i++){
-            x.add(Math.random()*0.5);
+
+    public void preencheVariaveis() {
+        for (int i = 0; i < 10; i++) {
+            x.add(Math.random() * 0.5);
             //System.out.println(x.get(i));
         }
-            
+
         //Preenchendo o Array w com valores aleatorios entre 0 e 1
-        for(int i=0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             w.add(Math.random());
             //System.out.println(w.get(i));
         }
-            
+
         //Preenchendo o Array de bias com valores aleatorios entre 0 e 0.2
-        for(int i=0; i<10; i++){
-            b.add(Math.random()*0.2);
+        for (int i = 0; i < 10; i++) {
+            b.add(Math.random() * 0.2);
             //System.out.println(b.get(i));
         }
-       
+    }
+    
+    public void treinaNeuronio() {
+        int count = 0;
+        //Preenchendo o Array x com valores de entrada
+
         double resultado = 0.0;
         double erro = 0.0;
         double desejado = 1.0;
         double alfa = 0.1;
-        
-        while(count!=1000){
-            for(int i=0; i<10; i++){
-                for(int j=0; j<10; j++){
+
+        while (count != 1000) {
+            for (int i = 0; i < w.size(); i++) {
+                for (int j = 0; j < w.size(); j++) {
                     resultado += (x.get(j) * w.get(j)) + b.get(j);
                     //y.add(i, (x.get(j) * w.get(j)) + b.get(j));
                 }
@@ -86,30 +89,32 @@ public class Neuron {
                 //Atualiza os pesos e os bias
                 erro = Math.abs(desejado - resultado);
                 //System.out.println("Erro eh: "+erro);
-                for(int j=0; j<10; j++){
-                    w.add(i+1, w.get(i) + (alfa * erro * x.get(j)));
-                    b.add(i+1, b.get(i) + alfa * erro);
-                    alfa = alfa*0.98;
+                for (int j = 0; j < 10; j++) {
+                    w.add(i + 1, w.get(i) + (alfa * erro * x.get(j)));
+                    b.add(i + 1, b.get(i) + alfa * erro);
+                    alfa = alfa * 0.98;
                 }
             }
-            for(int i=0; i<10; i++){
-                System.out.println("O peso numero "+i+" eh:"+ w.get(i));
+            for (int i = 0; i < 10; i++) {
+                System.out.println("O peso numero " + i + " eh:" + w.get(i));
             }
-            y=resultado*0.9;
-        count++;
-        System.out.println(count+" interacao");
+            y = resultado * 0.9;
+            count++;
+            System.out.println(count + " interacao");
         }
-    treinamento = true;        
+        treinamento = true;
     }
-    
-    public void gradienteDescendente(){
-        
+
+    public void backPropagation(ArrayList lista) {
+        //Esse metodo pega as saidas das n-1 camadas anteriores e aplica sobre forma de pesos
+        this.w.removeAll(x);  //Remove em caso de ter varios items
+        this.w = lista;
+        //Passa a lista de pesos das saidas dos neuronios anteriores para os pesos desse novo neuronio
     }
-    
-    public double funcaoAtivacao(double x){
+
+    public double funcaoAtivacao(double x) {
         double alfa = 0.3;
-        double f=1/(1+exp(-alfa*x));
+        double f = 1 / (1 + exp(-alfa * x));
         return f;
     }
 }
-    
